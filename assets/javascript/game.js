f2b822000849ccdedae0c864d7b4541b10d64a75
Dad <Dad>
2016-07-27 14:32:45 -0400

    ignore last change put the background in the font lolmaking sure heroku is up

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+/*
+	GOAL--> To make a trivia game that goes to new slides automatically via 2 methods
+		method 1. timer count down to 0
+		method 2. player selects answer
+
+	the player will have X amount of time to answer each question. 
+	the answer along with the correct answer will be displayed following a choise (or a time out)
+	then the game will move onto the next question
+	at the end of the game the amount of correct and incorrect aswers will be displayed (possibly with the average time to complete)
+*/
+/*
+	PLAN- make 2 arrays of objects
+
+	array 1 = question objects - store questions and functions for selecting questions
+	array 2 = answer objects - store 2 answer elements (page re-write) one for correct answers, one for wrong answers
+*/
+/*
+	EXTRA- Make different games based on difficulty can use less of the objects depending on which option picked
+*/
